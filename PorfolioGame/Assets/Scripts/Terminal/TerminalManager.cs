7fc06c8e322d769a80e2f8550b7769288718f0f6
basarekinci <basarekinci02@outlook.com>
2025-06-11 13:15:53 +0300

    Update

--- PorfolioGame/Assets/Scripts/Terminal/TerminalManager.cs
+++ PorfolioGame/Assets/Scripts/Terminal/TerminalManager.cs
@@ -1,10 +1,15 @@
-﻿using Data;
+﻿using System;
+using System.Threading.Tasks;
+using Data;
+using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Terminal
 {
     public class TerminalManager : MonoBehaviour
     {
+        [SerializeField] private TMP_Text loadingText;
         [SerializeField] private Transform terminalContentArea;
         [SerializeField] private CommandRespondDatabaseSo commandResponds;
         [SerializeField] private CommandSentEvent commandSentEvent;
@@ -12,6 +17,7 @@ namespace Terminal
         private void OnEnable()
         {
             commandSentEvent.RegisterListener(OnCommandSent);
+            ShowLoadingAnimation();
             //show the load animation and instantiate the base command
         }
         
@@ -23,6 +29,23 @@ namespace Terminal
         private void OnCommandSent(string cmd)
         {
             //instantiate the command response based on the command sent if the command matches any existing commands
+        }
+        
+        private async void ShowLoadingAnimation()
+        {
+            await Task.Delay(500);
+            TMP_Text text = Instantiate(loadingText, terminalContentArea);
+            int loadingCount = Random.Range(6, 10);
+            for (int i = 0; i < loadingCount; i++)
+            {
+                text.text = "\\";
+                await Task.Delay(TimeSpan.FromSeconds(0.15f));
+                text.text = "|";
+                await Task.Delay(TimeSpan.FromSeconds(0.15f));
+                text.text = "/";
+                await Task.Delay(TimeSpan.FromSeconds(0.15f));
+            }
+            Destroy(text.gameObject);
         }
     }
 }
